53963578be06cdf0b996e74ce7da26f96d78e5eb
yassinphilip <xaccrocheur@gmail.com>
2024-02-08 14:51:42 +0100

    child_process

--- js/server.js
+++ js/server.js
@@ -1,4 +1,5 @@
 const WebSocket = require('isomorphic-ws');
+const { exec } = require('child_process');
 
 const wss = new WebSocket.Server({ port: 8008 });
 
@@ -7,7 +8,24 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(message) {
     console.log('Received: %s', message);
+    
+    // Execute system command based on the received message
+    executeCommand(message);
   });
 
   ws.send('Hello, client!');
 });
+
+function executeCommand(command) {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Command stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Command stdout: ${stdout}`);
+  });
+}
